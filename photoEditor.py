fd853802acfee1d32b95b80e2f1499205a43b7be
MikeDurrant <mike_durrant@yahoo.co.uk>
2023-07-07 21:43:28 +0100

    commit with rotate and contrast

--- photoEditor.py
+++ photoEditor.py
@@ -8,7 +8,11 @@ pathOut = './editedImgs'
 for filename in os.listdir(path):
     img = Image.open(f"{path}/{filename}")
 
-    edit = img.filter(ImageFilter.SHARPEN).convert('L')
+    edit = img.filter(ImageFilter.SHARPEN).convert('L').rotate(-90)
+
+    factor = 1.1
+    enhancer = ImageEnhance.Contrast(edit)
+    edit = enhancer.enhance(factor)
 
     clean_name = os.path.splitext(filename)[0]
 
