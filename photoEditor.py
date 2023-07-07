700c42e973a1055d62613c7516a905328627f590
MikeDurrant <mike_durrant@yahoo.co.uk>
2023-07-07 20:43:18 +0100

    first commit

---
+++ photoEditor.py
@@ -0,0 +1,17 @@
+from PIL import Image, ImageEnhance, ImageFilter
+import os
+
+
+path = './imgs'
+pathOut = './editedImgs'
+
+for filename in os.listdir(path):
+    img = Image.open(f"{path}/{filename}")
+
+    edit = img.filter(ImageFilter.SHARPEN)
+
+    clean_name = os.path.splitext(filename)[0]
+
+    save_path = os.path.join(pathOut, f"{clean_name}_edited.jpg")
+
+    edit.save(save_path)
